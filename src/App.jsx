ae72202010ffb3cfdbb0e591cbe904ebae231e54
uizzuu <psypsy1346@naver.com>
2025-09-17 10:17:07 +0900

    상세페이지와 중첩라우팅 처리 완료

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import { useState } from "react";
 import Product from "./Product";
 import { Routes, Route } from "react-router-dom";
 import Detail from "./Detail";
+import About from "./About";
 
 function App() {
   // 상품정보를 갖는 product 스테이트를 만든다.
@@ -30,6 +31,11 @@ function App() {
         <Route path="/" element={<div>메인페이지</div>} />
         <Route path="/detail" element={<Detail/>} />
         <Route path="/cart" element={<div>장바구니페이지</div>} />
+        {/* 중첩 라우팅 처리 */}
+        <Route path="/about" element={<About />}>
+          <Route path="member" element={<div>Member</div>} />
+          <Route path="location" element={<div>Location</div>} />
+        </Route>
         <Route path="*" element={<div>Page Not Found 404 Error</div>}></Route>
       </Routes>
       {/* 상품진열영역 시작 */}
