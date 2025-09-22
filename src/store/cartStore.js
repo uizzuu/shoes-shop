1137252787b2bc30175f4ef357b6b27dadd41ab8
uizzuu <psypsy1346@naver.com>
2025-09-22 11:52:40 +0900

    immer 테스트 종료

--- src/store/cartStore.js
+++ src/store/cartStore.js
@@ -1,28 +1,56 @@
 import { create } from "zustand";
+import { immer } from "zustand/middleware/immer";
 
-const cartStore = create((set) => ({
-  cartData: [
-    { id: 0, name: "White and Black", count: 2 },
-    { id: 1, name: "Grey Nike", count: 1 },
-    { id: 2, name: "Red Knit", count: 5 },
-  ],
-  // 데이터 추가
-  addItem: (item) =>
-    set((state) => ({
-      cartData: [...state.cartData, item],
-    })),
-  // id 만 받아서 삭제 처리
-  removeItem: (id) =>
-    set((state) => ({
-      cartData: state.cartData.filter((x) => x.id !== id),
-    })),
-  // id 와 form 객체를 받아서 수정
-  updateItem: (id, updates) =>
-    set((state) => ({
-      cartData: state.cartData.map((x) =>
-        x.id === id ? { ...x, ...updates } : x
-      ),
-    })),
-}));
+const cartStore = create(
+  immer((set) => ({
+    cartData: [
+      { id: 0, name: "White and Black", count: 2 },
+      { id: 1, name: "Grey Nike", count: 1 },
+      { id: 2, name: "Red Knit", count: 5 },
+    ],
+    addItem: (item) =>
+        set((state) => {
+            state.cartData.push(item);
+        }),
+    removeItem: (id) => 
+        set((state) => {
+            const idx = state.cartData.findIndex((x)=> x.id === id);
+            if(idx !== -1) state.cartData.splice(idx, 1);
+        }),
+    updateItem: (id, updates) => 
+        set((state) => {
+            // 수정할 객체 먼저 찾기
+            const item = state.cartData.find((x)=> x.id === id);
+            if(item){
+                // name 이 공백이 아닌 경우
+                if(updates.name !== "") {
+                    item.name = updates.name;
+                }
+                // 수량이 숫자로 오면 변경
+                if(typeof updates.count === "number" && ! Number.isNaN(updates.count)){
+                    item.count = updates.count;
+                } 
+            }
+        })
+
+    //   데이터 추가
+    //   addItem: (item) =>
+    //     set((state) => ({
+    //       cartData: [...state.cartData, item],
+    //     })),
+    //   id 만 받아서 삭제 처리
+    //   removeItem: (id) =>
+    //     set((state) => ({
+    //       cartData: state.cartData.filter((x) => x.id !== id),
+    //     })),
+    //   id 와 form 객체를 받아서 수정
+    //   updateItem: (id, updates) =>
+    //     set((state) => ({
+    //       cartData: state.cartData.map((x) =>
+    //         x.id === id ? { ...x, ...updates } : x
+    //       ),
+    //     })),
+  }))
+);
 
 export default cartStore;
