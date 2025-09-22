b87b8ef2f32f19f2d936bbb586dcc53f852ff91f
uizzuu <psypsy1346@naver.com>
2025-09-22 10:20:47 +0900

    Form 생성

--- src/Cart/index.jsx
+++ src/Cart/index.jsx
@@ -3,12 +3,45 @@ import { Container } from "react-bootstrap";
 import Table from "react-bootstrap/Table";
 // import userStore from "../store/userStore";
 import cartStore from "../store/cartStore";
+import userStore from "../store/userStore";
+import { Form, Row, Col, Button, InputGroup } from "react-bootstrap";
 
 function Cart() {
-  const cartData = cartStore((x)=>x.cartData);
+  const userName = userStore((state) => state.userName);
+
+  // const cartData = cartStore((x)=>x.cartData);
+  const { cartData } = cartStore();
 
   return (
     <>
+    {/* CRUD 테스트용 Form */}
+    <Form className="mb-3 px-3">
+      <Row className="gy-2 gx-2 align-items-center">  
+        <Col xs={12} md={2}>
+        <Form.Label>ID</Form.Label>
+        <Form.Control name="id" type="number" placeholder="예 : 2" min={0} />
+        </Col>      
+        <Col xs={12} md={6}>
+        <Form.Label>상품명</Form.Label>
+        <Form.Control name="name" type="text" placeholder="예 : Red Nike Air" />
+        </Col>      
+        <Col xs={12} md={2}>
+        <Form.Label>수량</Form.Label>
+        <InputGroup>
+        <Form.Control name="count" type="number" placeholder="예 : 1" min={1} />
+        </InputGroup>
+        </Col>   
+        <Col xs={12} md={2} className="d-flex gap-2">
+        <Button variant="primary" className="flex-fill">추가</Button>
+        </Col>   
+        <Col xs={12} md={2} className="d-flex gap-2">
+        <Button variant="primary" className="flex-fill">수정</Button>
+        </Col>   
+        <Col xs={12} md={2} className="d-flex gap-2">
+        <Button variant="primary" className="flex-fill">삭제</Button>
+        </Col>   
+      </Row>
+    </Form>
       <Container>
         <Table>
           <thead>
@@ -20,13 +53,13 @@ function Cart() {
             </tr>
           </thead>
           <tbody>
-            {cartData.map((cart, i)=>(
+            {cartData.map((cart, i) => (
               <tr key={i}>
-              <td>{cart.id+1}</td>
-              <td>{cart.name}</td>
-              <td>{cart.count}</td>
-              <td>수정삭제</td>
-            </tr>
+                <td>{cart.id + 1}</td>
+                <td>{cart.name}</td>
+                <td>{cart.count}</td>
+                <td>수정삭제</td>
+              </tr>
             ))}
           </tbody>
         </Table>
--- src/store/userStore.js
+++ src/store/userStore.js
@@ -11,12 +11,22 @@ const userStore = create((set)=>(
         changeName: ()=>set((state)=>(
             { userName: '장원영' }
         )),
-        changeProductName: ()=>set((state)=>(
-            { productName: 'prada' }
+        addProduct: ()=>set((state)=>(
+            // state.productName : 이전 값
+            { 
+                productName: [...state.productName, '고무신'],
+                productStock: [...state.productStock, 1]
+             }
         )),
-        addProductName: ()=>set((state)=>(
-            { productName: [...productName, '고무신'] }
+        // 오버로딩 지원X, 똑같으면 나중것이 쓰임
+        addProduct: (name, stock=1)=>set((state)=>(
+            // state.productName : 이전 값
+            { 
+                productName: [...state.productName, name],
+                productStock: [...state.productStock, stock]
+             }
         )),
+        
     }
 ));
 
